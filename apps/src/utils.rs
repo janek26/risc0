60ed3d90c74d874c336447c258d0217c6d91937a
Janek <janek.rahrt@me.com>
2024-07-08 11:59:55 +0200

    storage proof

--- apps/src/utils.rs
+++ apps/src/utils.rs
@@ -1,13 +1,29 @@
-pub mod utils {
-    use zexe_algebra::Fp;
+use bitvec::prelude::{BitSlice, BitVec, Msb0};
+use bitvec::view::BitView;
+use eyre::{eyre, Result};
+use starknet::core::types::FieldElement;
 
-    pub fn to_field_element(x: u64) -> Result<Fp> {
-        let bytes = x.to_be_bytes();
-        Fp::from_bytes(&bytes).ok()
+pub fn felt_to_bits(felt: FieldElement) -> BitVec<u8, Msb0> {
+    felt.to_bytes_be().view_bits::<Msb0>()[5..].to_bitvec()
+}
+
+pub fn felt_from_bits(bits: &BitSlice<u8, Msb0>, mask: Option<usize>) -> Result<FieldElement> {
+    if bits.len() != 251 {
+        return Err(eyre!("expecting 251 bits"));
     }
 
-    pub fn to_u64(x: Fp) -> u64 {
-        let bytes = x.to_bytes();
-        u64::from_be_bytes(bytes.try_into().unwrap())
-    }
+    let mask = match mask {
+        Some(x) => {
+            if x > 251 {
+                return Err(eyre!("Mask cannot be bigger than 251"));
+            }
+            x
+        }
+        None => 0,
+    };
+
+    let mut bytes = [0u8; 32];
+    bytes.view_bits_mut::<Msb0>()[5 + mask..].copy_from_bitslice(&bits[mask..]);
+
+    FieldElement::from_bytes_be(&bytes).map_err(|e| eyre!(format!("{e}")))
 }
